d435d41e04e1e4b02006da96204881e027ebf8cd
Isabella <wileyisabella@gmail.com>
2021-03-14 15:54:43 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(names, event){
+    const cards = [];
+    for(let count = 0; count < names.length; count++){
+        cards.push(`Thank you, ${names[count]}, for the wonderful ${event} gift!`);
+    }
+    return cards;
+}
+
+function countDown(integer){
+    let num = integer;
+    while(num >= 0){
+        console.log(num);
+        num--;
+    }
+}
